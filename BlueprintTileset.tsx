63e71bf00961fef7598ff39c1326e60953207546
Peter Buckman <pjb252@cornell.edu>
2022-04-14 23:12:23 -0400

    created blueprint tileset

---
+++ BlueprintTileset.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="BlueprintTileset" tilewidth="65" tileheight="65" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="65" height="65" source="../block-0326-high contrast/block_ice-1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="65" height="65" source="../block-0326-high contrast/block_ice-3.png"/>
+ </tile>
+ <tile id="2">
+  <image width="65" height="65" source="../block-0326-high contrast/block_ice-4.png"/>
+ </tile>
+ <tile id="3">
+  <image width="65" height="65" source="../../Downloads/ice-blob.png"/>
+ </tile>
+ <tile id="4">
+  <image width="65" height="65" source="../block-0326-high contrast/block_redbrick-1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="65" height="65" source="../block-0326-high contrast/block_redbrick-2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="65" height="65" source="../block-0326-high contrast/block_redbrick-3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="65" height="65" source="../../Downloads/brick-blob.png"/>
+ </tile>
+ <tile id="8">
+  <image width="65" height="65" source="../block-0326-high contrast/block_stone-1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="65" height="65" source="../block-0326-high contrast/block_stone-5.png"/>
+ </tile>
+ <tile id="10">
+  <image width="65" height="65" source="../block-0326-high contrast/block_stone-6.png"/>
+ </tile>
+ <tile id="11">
+  <image width="65" height="65" source="../../Downloads/stone-blob.png"/>
+ </tile>
+ <tile id="12">
+  <image width="65" height="65" source="../block-0326-high contrast/block_wood-3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="65" height="65" source="../block-0326-high contrast/block_wood-1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="65" height="65" source="../block-0326-high contrast/block_wood-2.png"/>
+ </tile>
+ <tile id="15">
+  <image width="65" height="65" source="../../Downloads/wood-blob.png"/>
+ </tile>
+</tileset>
